6ad509add5c9d8ddd4682cb05494a80b21189ed3
Sousuyou <deramyh@gamil.com>
2016-04-25 13:49:48 +0900

    delite file  Please enter the commit message for your changes. Lines starting

--- fuel/app/views/contact/send.php
+++
@@ -1,7 +0,0 @@
-<ul class="nav nav-pills">
-	<li class='<?php echo Arr::get($subnav, "index" ); ?>'><?php echo Html::anchor('contact/index','Index');?></li>
-	<li class='<?php echo Arr::get($subnav, "confirm" ); ?>'><?php echo Html::anchor('contact/confirm','Confirm');?></li>
-	<li class='<?php echo Arr::get($subnav, "send" ); ?>'><?php echo Html::anchor('contact/send','Send');?></li>
-
-</ul>
-<p>Send</p>
